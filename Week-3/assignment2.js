2bf6504a44069f156643afa898a2e3556b390fbb
thiru1832 <rrthirumugilan18@gmail.com>
2023-02-23 22:53:28 +0530

    added new application

--- Week-3/assignment2.js
+++ Week-3/assignment2.js
@@ -1,13 +1,34 @@
-function abc (a,b){
-   console.log(a,b);
-   return function(a,b){
-    console.log(a,b);
+let customer = {
+   fName: "Sakar",
+   lName: "Chauhan",
+   city: "Dehradun",
+   state: "Uttarakhand",
+ };
+ 
+ let merchant = {
+   fName: "Jetha",
+   lName: "Lal",
+   city: "Surat",
+   state: "Gujrat",
+ };
+ 
+ function printBill(name,address) {
+     let bill = '';
+   if (name) {
+     bill+=`${this.fName} ${this.lName}. `;
    }
-}
-
-const a = abc(1,2);
-
-console.log(a);
-
-
-//a(5,6);
+   if (address) {
+     bill+= `Lives in ${this.city}, ${this.state}`;
+   }
+   return bill
+ }
+ 
+ const customerName = printBill.bind(customer, true , false);
+ console.log(typeof(customerName))
+ console.log(customerName());
+ 
+let abc = printBill.call(customer,false,true)
+console.log (typeof(abc));
+ console.log(printBill.call(customer,false,true));
+ 
+ console.log(printBill.apply(merchant, [true,true]));
---
+++ Week-3/demo.js
@@ -0,0 +1,7 @@
+let arr = [1,2,3,4];
+
+arr[5]=5;
+
+for(let i of arr){
+console.log(i);
+}
